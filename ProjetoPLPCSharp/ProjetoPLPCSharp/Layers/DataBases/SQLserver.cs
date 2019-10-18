011d14081b27dbee23347723fede54a3af3d0f8d
João Pedro <jpcabana1@gmail.com>
2019-10-18 12:04:55 -0300

    Banco
    Classe banco implementada

---
+++ ProjetoPLPCSharp/ProjetoPLPCSharp/Layers/DataBases/SQLserver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProjetoPLPCSharp.Layers.DataBases
+{
+    class SQLserver : IBanco
+    {
+       private string SqlData = "Data Source = 'localhost'; Initial Catalog = 'testeDB'; User='sa'; Password = 'otis2016'";
+       private SqlConnection Connection;
+       private SqlCommand comando;
+       private SqlDataAdapter Adapter;
+       
+       
+        public SQLserver()
+        {
+            this.Connection = new SqlConnection(SqlData);
+            this.Connection.Open();
+        }
+        public void ExecutaComando(string p_comando)
+        {
+            comando = new SqlCommand(p_comando, Connection);
+            comando.ExecuteNonQuery();
+        }
+
+        public DataSet ExecutaSelect(string p_comando)
+        {
+            DataSet Retorno;
+            try {             
+                Adapter = new SqlDataAdapter(p_comando, Connection);
+                Retorno = new DataSet();
+                Adapter.Fill(Retorno);
+                return Retorno;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+           
+            
+        }
+    }
+}
