c79d3e4185a80d87f58ca5f2f2fb32f73b92dcaf
João Pedro <jpcabana1@gmail.com>
2020-10-26 23:16:14 -0300

    Initial commit

---
+++ ProjetoBD2/ProjetoBD2/Layers/Views/InitialScreen.cs
@@ -0,0 +1,32 @@
+﻿using ProjetoPLPCSharp.Layers.DataBases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoPLPCSharp.Layers.Views
+{
+    public partial class InitialScreen : Form
+    {
+        public InitialScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void InitialScreen_Load(object sender, EventArgs e)
+        { 
+            FacBanco fac = new FacBanco();
+            IBanco banco = fac.CriarBanco("SQLServer");
+            DataSet dt = banco.ExecutaSelect("select * from Products");
+            BindingSource binding = new BindingSource();
+            binding.DataSource = dt.Tables[0];
+            binding.ResetBindings(true);
+            dataGridView1.DataSource = binding;
+        }
+    }
+}
---
+++ ProjetoBD2/ProjetoBD2/Models/ConfigModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBD2.Models
+{
+    class ConfigModel
+    {
+        public string DataSource { get; set; }
+        public string InitialCatalog { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("Data Source = '" + DataSource + "';");
+            stringBuilder.Append("Initial Catalog = '" + InitialCatalog + "';");
+            stringBuilder.Append("User = '" + User + "';");
+            stringBuilder.Append("Password = '" + Password + "';");
+            return stringBuilder.ToString();
+        }
+    }
+}
